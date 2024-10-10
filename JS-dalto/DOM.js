bcb08a55115741a5dd4bfc1a106bab06523b01ec
justmanuva <manuva2662004@gmail.com>
2024-10-11 00:55:44 +0200

    DOM avanzando

--- JS-dalto/DOM.js
+++ JS-dalto/DOM.js
@@ -11,9 +11,42 @@
 
 //FUNCIONES DE DOM
 
-//Seleccionar elemento por ID
-porId = document.getElementById("parrafo");
-document.write(porId);
-//Selecciona por tipo de elementos
-porTag = document.getElementsByTagName("p");
-document.write(porTag);
+//Selección de ELEMENTOS
+
+// Seleccionar elemento por ID
+// porId = document.getElementById("parrafo");
+// document.write(porId);
+// //Selecciona por tipo de elementos
+// porTag = document.getElementsByTagName("p");
+// document.write(porTag);
+// //Selecciona en query como le indiquemos (clase/ID...)
+// porClaseQuery = document.querySelector(".parrafo");
+// porIdQuery = document.querySelector("#rancio");
+// //Selecciona todos los nodos de la indicación, ya que se hace un array
+// porClaseQuery = document.querySelectorAll(".parrafo");
+// document.write(porClaseQuery[1])
+
+//Definir, Obtener y Elminiar Valores
+
+// const rangoEtario = document.querySelector(".rangoEtario");
+
+// //Cambia un atributo (en el param1 pon param2)
+// rangoEtario.setAttribute("type","range");
+// //Obtiene contenido del atributo
+// document.write(rangoEtario.getAttribute("type"));
+// //Borrar el valor del atributo
+// document.write(rangoEtario.removeAttribute("type"));
+
+//Atributos comunes de HTML
+
+const titulo = document.querySelector(".titulo");
+//contentEditable para cambiar el contenido.
+titulo.setAttribute("contentEditable","true");
+//Cambiar la dirección de la escritura (LeftToRight (lft), RightToLeft (rtl))
+titulo.setAttribute("dir","ltr");
+//Ocultar/desocultar elemento
+// titulo.setAttribute("hidden","false");
+//Para poder hacer focus a un elemento
+titulo.setAttribute("tabindex","0");
+//Cambia el title (nombre que sale al pasar el raton por encima de un elemento)
+titulo.setAttribute("title","jajajaja xd");
